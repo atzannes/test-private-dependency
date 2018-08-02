c039bba91aa1d95c8d1b992b798c0047db50f04d
Alexandros Tzannes <alex.tzannes@altschool.com>
2018-08-01 17:05:12 -0700

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
-import os
 from setuptools import (
     find_packages,
     setup,
 )
 
-VERSION = '0.1'
-
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 setup(
     name='test-private-dependency',
-    version=VERSION,
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    long_description=README,
+    long_description='just for testing dependency links work',
     dependency_links=[],
     install_requires=['requests']
 )
